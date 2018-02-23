856e71b11e5056679ca6e1fa92ad76d677a4e8cf
Imran <imran.wpsi@gmail.com>
2018-02-23 21:56:11 +0600

    initial commit.

---
+++ resources/assets/js/components/Customer.vue
@@ -0,0 +1,12 @@
+<template>
+	<tr>
+	  <td>{{ customer.name }}</td>
+	  <td>{{ customer.email }}</td>
+	</tr>
+</template>
+
+<script>
+	export default{
+		props:['customer']
+	}
+</script>
---
+++ resources/assets/js/components/Customers.vue
@@ -0,0 +1,36 @@
+<template>          
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <customer v-for="customer in customers" v-bind:customer="customer"></customer>
+    </tbody>
+  </table>     
+</template>
+
+<script>
+    import customer from './Customer.vue';
+    export default {
+        data(){
+        	return{
+        		customers: [
+        			// {name:'Imran Hossain',email:'imran@gmail.com'},
+        			// {name:'Emon',email:'emon@gmail.com'}
+        		],
+        	}
+        },
+        components:{customer},
+        created(){
+        	this.fetchCustomer();
+        },
+        methods:{
+        	fetchCustomer(){
+        		this.$http.get("customer").then(response => {this.customers = response.data.customers});
+        	}
+        }
+    }
+</script>
